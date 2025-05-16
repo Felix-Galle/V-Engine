77ab17368571ac6811394669734db26963f3b8e9
Felix-Galle <felixjohngalle@gmail.com>
2025-05-16 14:53:36 +0200

    renamed token.py to custom_token.py due to confliction with stdlib

--- v_engine.py
+++ v_engine.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
-"""
-V Engine: A robust DSL interpreter for V-language game scripts.
-Features:
-- Indentation-based DSL parsing
-- Scenes, entities, events (start, update, key_<Key>, collision)
-- Actions: spawn, destroy, set, change_scene
-- Solid-colour or image backgrounds
-- Shape-based entities (rectangle, oval)
-- 60fps game loop
-"""
 
 import sys
 from lexer import Lexer
